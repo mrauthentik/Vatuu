7c1ca06da8d3cd9e9766907a54106dc9044cafe5
mrauthentik <umokeuchenna2020@gmail.com>
2024-12-31 12:51:46 +0100

    installed axios and fetch data

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,7 +1,45 @@
-import { FC } from "react";
+import axios from 'axios'
+import { useState, FC, useEffect } from 'react';
 
-const App: FC = () => {
-  return <div>App</div>;
-};
 
-export default App;
+interface AppProps {
+  title: string;
+}
+
+const App: FC<AppProps> = ({title}) =>{
+  
+  
+  return (
+     <div>
+       <h1> {title}</h1>
+     </div>
+   )
+
+
+}
+
+// import axios from 'axios';
+// import { FC, useEffect } from 'react';
+
+// interface AppProps {
+//   title: string;
+// }
+
+// const App: FC<AppProps> = ({ title }) => {
+//   useEffect(() => {
+//     const getUsers = async () => {
+//       try {
+//         const { data } = await axios.get(
+//           'https://randomuser.me/api/?results=10'
+//         );
+//         console.log(data);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+//     getUsers();
+//   }, []);
+
+//   return <h1>{title}</h1>;
+// }
+export default App
